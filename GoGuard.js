e17a3b6fb364b2b6546523b9f24a081c4565d389
Prospero <angeloprospero@hotmail.com>
2022-12-07 13:29:30 -0300

    Aplicativo GoGuard de calculo de VA por dias trabalhados

---
+++ GoGuard.js
@@ -0,0 +1,11 @@
+
+function calcula(){
+
+    var vale = document.getElementById('vale').value
+    var dias = document.getElementById('dias').value
+   
+
+    var total = document.getElementById('total')
+    total.innerHTML = dias*vale
+
+}
